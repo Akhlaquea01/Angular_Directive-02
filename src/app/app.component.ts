cae793d5188573f07ff8c9be5dadbfc5abbd3d83
Akhlaque <akhlaquea01@gmail.com>
2021-03-23 15:39:19 +0530

    ngif ngelse ngFor ngSwitch

---
+++ src/app/app.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'directives';
+  show = false;
+  show1 = 'yes'
+  user='firoz'
+  color = ""
+  isavailable2=false
+  data = ['Firoz', 'Sultan', 'Akhlaque', 'Dude']
+  data1 = [
+    {
+      name: 'Firoz khan',
+      age: 23,
+      work:'MEAN Developer'
+    },
+    {
+      name: 'Firoz khan2',
+      age: 23,
+      work:'MEAN Developer'
+    },
+    {
+      name: 'Akhlaque',
+      age: 23,
+      work:'MEAN Developer'
+    },
+    {
+      name: 'Asfaque',
+      age: 23,
+      work:'Data Scientist'
+    }
+  ]
+  changeColor(){
+    this.color='color'
+  }
+}
